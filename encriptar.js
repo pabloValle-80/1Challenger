96407ddb5226e478c41b0dd83fac32fec62885c6
Pablo Valle <chanchovalle80@gmail.com>
2023-01-27 09:45:55 -0300

    Repositorio del primer Challenger iniciado, primer commit

---
+++ encriptar.js
@@ -0,0 +1,52 @@
+function encriptar() {
+  var texto = document.querySelector("#encriptarArea").value;
+  var textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+  document.querySelector(".text-input-salida").value = textoCifrado;
+  document.querySelector("#encriptarArea").value;
+  
+  document.getElementById("muñeco").style.display = "none";
+  document.getElementById("msg-ningun").style.display = "none";
+  document.getElementById("msg-ingrese").style.display = "none";
+
+  document.getElementById("copiar").style.display="block";
+  
+}
+
+var boton1 = document.querySelector("#btn-encriptar");
+boton1.onclick = encriptar;
+
+function desencriptar() {
+  var texto = document.querySelector("#encriptarArea").value;
+  var textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  document.querySelector(".text-input-salida").value = textoCifrado;
+  document.querySelector("#encriptarArea").value;
+
+  document.getElementById("muñeco").style.display = "none";
+  document.getElementById("msg-ningun").style.display = "none";
+  document.getElementById("msg-ingrese").style.display = "none";
+
+  document.getElementById("copiar").style.display="block";
+}
+
+var boton2 = document.querySelector("#btn-Desencriptar");
+boton2.onclick = desencriptar;
+
+
+
+function copy() {
+  let copyText = document.getElementById ("msg");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+document.getElementById("btnCopiar").addEventListener("click", copy);
